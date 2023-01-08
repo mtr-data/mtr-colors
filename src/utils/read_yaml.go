3d70d7718cd96efacf0a30d489b1d69c7d108e26
seltrac <76126473+seltrac@users.noreply.github.com>
2023-01-08 09:23:54 +0800

    chore: use ioutil package

--- src/utils/read_yaml.go
+++ src/utils/read_yaml.go
@@ -1,27 +1,13 @@
 package utils
 
 import (
-	"os"
+	"io/ioutil"
 
 	"gopkg.in/yaml.v3"
 )
 
 func ReadFileAsBytes(filename string) (data []byte, err error) {
-	file, err := os.Open(filename)
-	if err != nil {
-		return nil, err
-	}
-
-	fileInfo, err := file.Stat()
-	if err != nil {
-		return nil, err
-	}
-
-	size := fileInfo.Size()
-	buf := make([]byte, size)
-	_, err = file.Read(buf)
-
-	return buf, err
+	return ioutil.ReadFile(filename)
 }
 
 func ReadFileAsYaml(filename string) (out interface{}, err error) {
